85e01fba9f5bad178accdda66bdf58196912029c
Andres <afgalvan@unicesar.edu.co>
2021-09-06 22:10:07 +0100

    chore: add simple greeting service

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # GrpcDemo
 
 [![.NET](https://github.com/afgalvan/GrpcDemo/actions/workflows/dotnet.yml/badge.svg)](https://github.com/afgalvan/GrpcDemo/actions/workflows/dotnet.yml)
-[![NuGet](https://github.com/afgalvan/GrpcDemo/actions/workflows/publish.yml/badge.svg)](https://github.com/afgalvan/GrpcDemo/actions/workflows/publish.yml)
-[![Nuget Version](https://img.shields.io/nuget/v/GrpcDemo.Template?label=version&logo=NuGet&labelColor=22272E&color=blue)](https://www.nuget.org/packages/GrpcDemo.Template/)
 
 Basic C# project structure.
 
---
+++ src/Greetings/Greetings.Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Greetings.Service;
+using Grpc.Core;
+
+namespace Greetings.Client
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string url = "localhost:5000";
+            var channel = new Channel(url, ChannelCredentials.Insecure);
+            var client  = new Greeter.GreeterClient(channel);
+
+            HelloReply helloResponse = await client.SayHelloAsync(
+                new HelloRequest { Name = "Andres" }
+            );
+            Console.WriteLine(helloResponse.Message);
+        }
+    }
+}
